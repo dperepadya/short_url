b541d4ededb51bc08310c7912d2923de8224db6b
Dmitry Perepadya <dperepadya@gmail.com>
2024-08-09 12:52:12 +0000

    09Aug2024

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import random
 import string
 import validators
-from fastapi import FastAPI, Request, Form, Path, Query, HTTPException
+from fastapi import FastAPI, Request, Form, Query, HTTPException
 from fastapi.responses import HTMLResponse, RedirectResponse
 from starlette.templating import Jinja2Templates
+
+from file_worker import FileWorker
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+FILE_PATH = "url_map.json"
+file_worker = FileWorker(FILE_PATH)
+
 def get_short_url():
     return ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(6))
 
-def get_original_url_from_db(short_url):
+async def get_original_url_from_db(short_url):
     if short_url is None or len(short_url) == 0:
         return None
-    url = "https://www.google.com/"
-    return url
+    url_map = await file_worker.load()
+    return url_map.get(short_url, None)
 
 @app.get("/", response_class=HTMLResponse)
 async def get_url(request: Request, error: str = Query(None)):
@@ -25,13 +30,19 @@ async def get_url(request: Request, error: str = Query(None)):
 async def post_url(request: Request, url: str = Form(...)):
     if not validators.url(url):
         return RedirectResponse(url="/?error=Invalid URL", status_code=302)
-    url_dict = {"url": url, "short_url": get_short_url()}
+    short_url = get_short_url()
+    if not short_url:
+        raise HTTPException(status_code=500, detail=f"Cannot get Short url for {url}")
+    url_map = await file_worker.load()
+    url_map[short_url] = url
+    await file_worker.save(url_map)
+    url_dict = {"url": url, "short_url": short_url}
     return templates.TemplateResponse(request=request, name="result.html", context={"url_dict": url_dict})
 
 @app.get("/{url:path}")
 async def get_original_url(url: str):
-    print(url)
-    original_url = get_original_url_from_db(url)
+    original_url = await get_original_url_from_db(url)
+    print(original_url)
     if not original_url or not validators.url(original_url):
         raise HTTPException(status_code=404, detail=f"URL {original_url} not found")
     return RedirectResponse(url=original_url)
